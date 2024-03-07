0049864b3776b97b2d03465f1c70757cd339e988
CHAEYULL <leopportunity5@gmail.com>
2024-03-07 16:08:20 +0900

    테이블 생성

--- src/main/java/com/whizpath/nutritionreport/nutritionreport/NutritionreportApplication.java
+++ src/main/java/com/whizpath/nutritionreport/nutritionreport/NutritionreportApplication.java
@@ -1,10 +1,14 @@
 package com.whizpath.nutritionreport.nutritionreport;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.whizpath.nutritionreport.nutritionreport.model.Nutrition;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
@@ -13,6 +17,7 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @SpringBootApplication
 public class NutritionreportApplication {
@@ -20,10 +25,57 @@ public class NutritionreportApplication {
 	public static void main(String[] args) throws JRException{
 		SpringApplication.run(NutritionreportApplication.class, args);
 		String filePath = "C:\\CR-R\\Spring\\nutritionreport\\src\\main\\resources\\templates\\nutritionreport.jrxml";
+
+		Nutrition protein = new Nutrition("Protein", 62, 83, "g");
+		Nutrition carbonhydrates = new Nutrition("Protein", 62, 83, "g");
+		Nutrition fiber = new Nutrition("Protein", 62, 83, "g");
+		Nutrition sugars = new Nutrition("Protein", 62, 83, "g");
+		Nutrition fat = new Nutrition("Protein", 62, 83, "g");
+		Nutrition cholesterol = new Nutrition("Protein", 62, 83, "g");
+		Nutrition sodium = new Nutrition("Protein", 62, 83, "g");
+		Nutrition potassium = new Nutrition("Protein", 62, 83, "g");
+		Nutrition calcium = new Nutrition("Protein", 62, 83, "g");
+		Nutrition iron = new Nutrition("Protein", 62, 83, "g");
+		Nutrition apple = new Nutrition("Protein", 62, 83, "g");
+		Nutrition banana = new Nutrition("Protein", 62, 83, "g");
+		Nutrition melon = new Nutrition("Protein", 62, 83, "g");
+		Nutrition watermelon = new Nutrition("Protein", 62, 83, "g");
+		Nutrition peach = new Nutrition("Protein", 62, 83, "g");
+		Nutrition grape = new Nutrition("Protein", 62, 83, "g");
+		Nutrition milk = new Nutrition("Protein", 62, 83, "g");
+		Nutrition chocomilk = new Nutrition("Protein", 62, 83, "g");
+		Nutrition coffeemilk = new Nutrition("Protein", 62, 83, "g");
+		Nutrition juice = new Nutrition("Protein", 62, 83, "g");
+		
+		List<Nutrition> nutritionList = new ArrayList<>();
+		nutritionList.add(protein);
+		nutritionList.add(carbonhydrates);
+		nutritionList.add(fiber);
+		nutritionList.add(sugars);
+		nutritionList.add(fat);
+		nutritionList.add(cholesterol);
+		nutritionList.add(sodium);
+		nutritionList.add(potassium);
+		nutritionList.add(calcium);
+		nutritionList.add(iron);
+		nutritionList.add(apple);
+		nutritionList.add(banana);
+		nutritionList.add(melon);
+		nutritionList.add(watermelon);
+		nutritionList.add(peach);
+		nutritionList.add(grape);
+		nutritionList.add(milk);
+		nutritionList.add(chocomilk);
+		nutritionList.add(coffeemilk);
+		nutritionList.add(juice);
+
+		JRBeanCollectionDataSource nutritionDataSource = new JRBeanCollectionDataSource(nutritionList);
+
 		Map<String, Object> parametaters = new HashMap<>(); 
 		parametaters.put("firstName", "John");
 		parametaters.put("dob", "07/09/2020");
 		parametaters.put("age", 20);
+		parametaters.put("nutritionDataSet", nutritionDataSource);
 		JasperReport report = JasperCompileManager.compileReport(filePath);
 		JasperPrint print = JasperFillManager.fillReport(report, parametaters, new JREmptyDataSource());
 		JasperExportManager.exportReportToPdfFile(print,"C:\\CR-R\\Spring\\nutritionreport\\src\\main\\resources\\static\\nutritionreport.pdf" );
