df5e2bc63077f8e90592bb4d81d6c223e1b84221
leo <leopportunity@gmail.com>
2024-03-08 16:12:40 +0900

    서브 차트 생성

--- src/main/java/com/whizpath/nutritionreport/nutritionreport/NutritionreportApplication.java
+++ src/main/java/com/whizpath/nutritionreport/nutritionreport/NutritionreportApplication.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.whizpath.nutritionreport.nutritionreport.model.Food;
 import com.whizpath.nutritionreport.nutritionreport.model.MacroNutrient;
 import com.whizpath.nutritionreport.nutritionreport.model.Nutrition;
 
@@ -89,6 +90,8 @@ public class NutritionreportApplication {
 		parametaters.put("age", 20);
 		parametaters.put("nutrtitionDataSet", nutritionDataSource);
 		parametaters.put("macroNutrientDataSource", macroNutrientDataSource);
+		parametaters.put("foodReport", getFoodReport() );
+		parametaters.put("foodParameter", getFoodParameter());
 		JasperReport report = JasperCompileManager.compileReport(filePath);
 		JasperPrint print = JasperFillManager.fillReport(report, parametaters, new JREmptyDataSource());
 		JasperExportManager.exportReportToPdfFile(print,"/Users/fergus/Documents/CR-Project/Spring/nutritionreport-with-jasper/src/main/resources/static/nutritionreport.pdf" );
@@ -96,4 +99,47 @@ public class NutritionreportApplication {
 		
 	}
 
+	private static JRBeanCollectionDataSource getFoodDataSource(){
+		List<Food> foodList = new ArrayList<>();
+		Food banana = new Food("banana", "breakfast",22,13,3);
+		Food avocado = new Food("avocado", "brakfast",8,12,8);
+		Food milk = new Food("milk","breakfast",8,12,8);
+		Food chicken = new Food("chicken","lunch",2,0,26);
+		Food rice = new Food("rice", "lunch", 0,25,26);
+		Food egg = new Food("egg", "breakfast",5,0,6);
+		Food potato = new Food("potato", "lunch",5,37,4);
+		Food oats = new Food("oats","dinner",5,51,13);
+		
+		foodList.add(banana);
+		foodList.add(avocado);
+		foodList.add(milk);
+		foodList.add(chicken);
+		foodList.add(rice);
+		foodList.add(egg);
+		foodList.add(potato);
+		foodList.add(oats);
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(foodList);
+		return dataSource;
+	}
+
+	private static Map getFoodParameter(){
+		Map<String, Object> foodParameter = new HashMap<>();
+		foodParameter.put("foodDataset", getFoodDataSource());
+		return foodParameter;
+	}
+
+	private static JasperReport getFoodReport(){
+		String filPath = "/Users/fergus/Documents/CR-Project/Spring/nutritionreport-with-jasper/src/main/resources/templates/food_nutrition.jrxml";
+		JasperReport report = null;
+		try {
+			report = JasperCompileManager.compileReport(filPath);
+		} catch (JRException e) {
+			throw new RuntimeException(e);
+		}
+		return report;
+	}
+
+
 }
+
+
